697c7a07564bcfbf9f6620d2a180dcf4c6a9506b
Luis Parcon <luisp@macbook-pro.home>
2022-12-25 14:41:18 +0000

    completing day 3 part1

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -4,60 +4,66 @@
 #include <map>
 #include <string>
 
-std::string compare(std::string a, std::string b, int index)
+int compare(std::string a, std::string b, std::map<char, int> priority)
 {
-    std::string letter;
-    for (int i = 0; i < b.length(); i++)
-    {
-        if (a.c_str()[index] == b.c_str()[i])
+    int priorityScore = 0;
+        char commonLetter;
+
+        for (int i = 0; i < a.length(); i++)
         {
-            std::cout<<"found "<<a.c_str()[index]<<std::endl;
-            
+            for (int j = 0; j < b.length(); j++)
+            {
+                if (a[i] == b[j])
+                {
+                    commonLetter = a[i];
+                }
+            }
         }
 
-    }
-    if (index != a.length())
-    {
-        compare(a,b, ++index);
-    }
-    return "";
+        priorityScore += a[commonLetter];
+    return priorityScore;
 
-}
+    }
 
-std::unordered_map<std::string, std::string> parse(std::string filepath){
-    int length;
-    int id = 1;
-    std::string data;
-    std::string aCompartment;
-    std::string bCompartment;
-    std::ifstream input(filepath);
-    std::unordered_map<std::string, std::string> rucksacks; 
 
-    // Load the data
-    while (getline(input, data))
+
+int partA(std::string filepath, std::map<char,int> priority){
+    std::string inputLine;
+    std::ifstream input (filepath);
+    int priorityScore = 0;
+
+    while (getline(input, inputLine))
     {
-        length = data.length();
-        aCompartment = data.substr(0, length/2);
-        bCompartment = data.substr(length/2, length);
-        rucksacks.insert(std::pair<std::string, std::string>(aCompartment,bCompartment));
+
+        // store first half of input line in a string and second half in a string
+        // for example if there are 12 characters in the input line, the first half will be 6 characters and the second half will be 6 characters from the next character
+        std::string compartmentA = inputLine.substr(0, inputLine.length() / 2);
+        std::string compartmentB = inputLine.substr(inputLine.length() / 2, inputLine.length() / 2);
+        // find the common letter between the two halfs
+        char commonLetter;
+
+        for (int i = 0; i < compartmentA.length(); i++)
+        {
+            for (int j = 0; j < compartmentB.length(); j++)
+            {
+                if (compartmentA[i] == compartmentB[j])
+                {
+                    commonLetter = compartmentA[i];
+                }
+            }
+        }
+
+        priorityScore += priority[commonLetter];
     }
-    std::string line;
-    std::map<int, std::string> bagPriority;
-    std::ifstream priority("priority");
-    while (getline(priority, line))
-    {
-        bagPriority.insert(std::pair<int, std::string>(id,line));
-    }
-    std::unordered_map<std::string, std::string>::iterator it;
-    for (it = rucksacks.begin(); it != rucksacks.end(); it++)
-    {
-        compare(it->first,it->second, 0);
-    }
-    return rucksacks;
+
+    std::cout << priorityScore;
+    return priorityScore;
 }
 
 int main()
 {
-    parse("data");
+    std::map<char, int> priorities = {{'a', 1}, {'b', 2}, {'c', 3}, {'d', 4}, {'e', 5}, {'f', 6}, {'g', 7}, {'h', 8}, {'i', 9}, {'j', 10}, {'k', 11}, {'l', 12}, {'m', 13}, {'n', 14}, {'o', 15}, {'p', 16}, {'q', 17}, {'r', 18}, {'s', 19}, {'t', 20}, {'u', 21}, {'v', 22}, {'w', 23}, {'x', 24}, {'y', 25}, {'z', 26}, {'A', 27}, {'B', 28}, {'C', 29}, {'D', 30}, {'E', 31}, {'F', 32}, {'G', 33}, {'H', 34}, {'I', 35}, {'J', 36}, {'K', 37}, {'L', 38}, {'M', 39}, {'N', 40}, {'O', 41}, {'P', 42}, {'Q', 43}, {'R', 44}, {'S', 45}, {'T', 46}, {'U', 47}, {'V', 48}, {'W', 49}, {'X', 50}, {'Y', 51}, {'Z', 52}};
+    int sum = partA("data", priorities);
+    std::cout<<sum<<std::endl;
     return 0;
 }
